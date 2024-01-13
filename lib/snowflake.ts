b5c65f99a56e2fb50fed3c1fe237d6b8fb481787
WerdoxDev <matin.tat85@gmail.com>
2024-01-13 17:35:17 +0100

    Added a .gitignore

--- lib/snowflake.ts
+++ lib/snowflake.ts
@@ -2,9 +2,11 @@ import { Snowflake } from "@sapphire/snowflake";
 
 const epoch = new Date("2023-01-01T00:00:00.000Z");
 
+const globalSnowflake = new Snowflake(epoch);
+
 export const snowflake = {
    generate() {
-      const snowflake = new Snowflake(epoch);
-      return snowflake.generate().toString();
+      const value = globalSnowflake.generate();
+      return value.toString();
    },
 };
