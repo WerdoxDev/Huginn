be28b3d4fe62b93b1e36f1dd490874bd6cfe67ef
Matin Tat <matin.tat85@gmail.com>
2024-05-25 18:28:46 +0200

    Added Splashscreen and minor changes to settings modal

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,47 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{utils::config::AppUrl, Manager, Window, WindowUrl};
+
+#[tauri::command]
+async fn open_main(window: Window) {
+    // Show main window
+    window
+        .get_window("main")
+        .expect("no window labeled 'main' found")
+        .show()
+        .unwrap();
+}
+
+#[tauri::command]
+async fn hide_splashscreen(window: Window) {
+    // Hide splashscreen window
+    window
+        .get_window("splashscreen")
+        .expect("no window labeled 'splashscreen' found")
+        .hide()
+        .unwrap()
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let port = 3002;
+
+    let mut context = tauri::generate_context!();
+    let url = format!("http://localhost:{}", port).parse().unwrap();
+    let window_url = WindowUrl::External(url);
+    // rewrite the config so the IPC is enabled on this URL
+    context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
+
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![open_main, hide_splashscreen])
+        .plugin(tauri_plugin_localhost::Builder::new(port).build())
+        .build(context)
+        // .run(context)
+        .expect("error while running tauri application")
+        .run(|_, e| match e {
+            tauri::RunEvent::Updater(event) => {
+                dbg!(event);
+            }
+            _ => (),
+        });
 }
