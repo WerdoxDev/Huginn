aaa23c8b1683021c0803beb8dc9d3887680527a5
Matin Tat <matin.tat85@gmail.com>
2024-08-01 11:57:07 +0200

    Testing different config for vercel

--- packages/huginn-server/src/index.ts
+++ packages/huginn-server/src/index.ts
@@ -1,6 +1,7 @@
 import consola from "consola";
 import { version } from "../package.json";
 import { startListening } from "./commands";
+import { startServer } from "./server";
 
 const connectionString = process.env.MONGODB_CONNECTION_STRING;
 export const cdnRoot = process.env.CDN_ROOT;
@@ -9,22 +10,19 @@ export const serverPort = process.env.SERVER_PORT;
 export const certFile = process.env.CERTIFICATE_PATH && Bun.file(process.env.CERTIFICATE_PATH);
 export const keyFile = process.env.PRIVATE_KEY_PATH && Bun.file(process.env.PRIVATE_KEY_PATH);
 
-async function main() {
-   if (!connectionString) {
-      consola.error("Database config is not set correctly!");
-      return;
-   }
-
-   if (!serverHost || !serverPort) {
-      consola.error("Server config is not set correctly!");
-      return;
-   }
-
-   consola.info(`Using version ${version}`);
-
-   await import("./server");
+if (!connectionString) {
+   consola.error("Database config is not set correctly!");
+   process.exit();
 }
 
-await main();
+if (!serverHost || !serverPort) {
+   consola.error("Server config is not set correctly!");
+   process.exit();
+}
 
-await startListening();
+consola.info(`Using version ${version}`);
+
+// await startListening();
+
+const server = startServer();
+export default server;
--- packages/huginn-server/src/server.ts
+++ packages/huginn-server/src/server.ts
@@ -13,83 +13,90 @@ import testRoute from "./routes/test-routes";
 import { TokenInvalidator } from "./token-invalidator";
 import "./db/index";
 
-consola.start("Starting server...");
+export function startServer() {
+   consola.start("Starting server...");
 
-const app = new Hono();
+   const app = new Hono();
 
-app.use("*", cors());
+   app.use("*", cors());
 
-app.use("*", async (c, next) => {
-   if (c.req.method === "OPTIONS") {
-      return;
-   }
+   app.use("*", async (c, next) => {
+      if (c.req.method === "OPTIONS") {
+         return;
+      }
 
-   logRequest(c.req.path, c.req.method, await tryGetBodyJson(c.req));
+      logRequest(c.req.path, c.req.method, await tryGetBodyJson(c.req));
 
-   await next();
+      await next();
 
-   const response = c.res.clone();
+      const response = c.res.clone();
 
-   if (c.res.status >= 200 && c.res.status < 300) {
-      logResponse(c.req.path, c.res.status, await tryGetBodyJson(response));
-   } else if (c.res.status === 500) {
-      logReject(c.req.path, c.req.method, "Server Error", c.res.status);
-   } else {
-      logReject(c.req.path, c.req.method, (await tryGetBodyJson(response)) as HuginnErrorData, c.res.status);
-   }
-});
+      if (c.res.status >= 200 && c.res.status < 300) {
+         logResponse(c.req.path, c.res.status, await tryGetBodyJson(response));
+      } else if (c.res.status === 500) {
+         logReject(c.req.path, c.req.method, "Server Error", c.res.status);
+      } else {
+         logReject(c.req.path, c.req.method, (await tryGetBodyJson(response)) as HuginnErrorData, c.res.status);
+      }
+   });
 
-app.onError((e, c) => {
-   logServerError(c.req.path, e);
+   app.onError((e, c) => {
+      logServerError(c.req.path, e);
 
-   if (e instanceof SyntaxError) {
-      return error(c, createError(Error.malformedBody()), HttpCode.BAD_REQUEST);
-   }
+      if (e instanceof SyntaxError) {
+         return error(c, createError(Error.malformedBody()), HttpCode.BAD_REQUEST);
+      }
 
-   // logReject(c.req.path, c.req.method, e.message);
+      // logReject(c.req.path, c.req.method, e.message);
 
-   return serverError(c, e, false);
-});
+      return serverError(c, e, false);
+   });
 
-app.route("/", routes);
-app.route("/", testRoute);
+   app.route("/", routes);
+   app.route("/", testRoute);
 
-app.get("/", c => {
-   return c.html(
-      '<div style="height:100%; display: flex; align-items:center; justify-content:center;"><div style="font-size: 2rem;">Huginn API Homepage</div></div>',
-   );
-});
+   app.get("/", c => {
+      return c.html(
+         '<div style="height:100%; display: flex; align-items:center; justify-content:center;"><div style="font-size: 2rem;">Huginn API Homepage</div></div>',
+      );
+   });
+
+   // const server = Bun.serve<string>({
+   //    cert: certFile,
+   //    key: keyFile,
+   //    port: serverPort,
+   //    hostname: serverHost,
+   //    fetch(req, server) {
+   //       const url = new URL(req.url);
+   //       if (url.pathname === "/gateway") {
+   //          if (server.upgrade(req)) {
+   //             return;
+   //          }
+   //          return new Response(JSON.stringify(createError(Error.websocketFail())), { status: HttpCode.BAD_REQUEST });
+   //       }
+
+   //       return app.fetch(req, server);
+   //    },
+   //    websocket: {
+   //       open: ws => {
+   //          gateway.onOpen(ws);
+   //       },
+   //       close: (ws, code, reason) => {
+   //          gateway.onClose(ws, code, reason);
+   //       },
+   //       message: (ws, message) => gateway.onMessage(ws, message),
+   //       sendPings: false,
+   //    },
+   // });
+
+   const server = { port: serverPort, hostname: serverHost, fetch: app.fetch };
+
+   consola.success("Server started!");
+   // consola.box(`Listening on ${colors.green(server.url.href)}`);
+   consola.box(`Listening on ${colors.green("/* todo */")}`);
+
+   return server;
+}
 
 export const gateway = new ServerGateway({ logHeartbeat: false });
 export const tokenInvalidator = new TokenInvalidator();
-
-export const server = Bun.serve<string>({
-   cert: certFile,
-   key: keyFile,
-   port: serverPort,
-   hostname: serverHost,
-   fetch(req, server) {
-      const url = new URL(req.url);
-      if (url.pathname === "/gateway") {
-         if (server.upgrade(req)) {
-            return;
-         }
-         return new Response(JSON.stringify(createError(Error.websocketFail())), { status: HttpCode.BAD_REQUEST });
-      }
-
-      return app.fetch(req, server);
-   },
-   websocket: {
-      open: ws => {
-         gateway.onOpen(ws);
-      },
-      close: (ws, code, reason) => {
-         gateway.onClose(ws, code, reason);
-      },
-      message: (ws, message) => gateway.onMessage(ws, message),
-      sendPings: false,
-   },
-});
-
-consola.success("Server started!");
-consola.box(`Listening on ${colors.green(server.url.href)}`);
