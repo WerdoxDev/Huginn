d656874fb8a52ecd07c0cf418d628b052b61f8e2
Mahziyar Farahmandian <Mahziar84@gmail.com>
2024-08-31 20:07:37 +0330

    Website added

---
+++ packages/huginn-website/src/components/HeaderButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+const buttonProps = defineProps<{
+    link: string,
+    text: string,
+}>()
+
+</script>
+
+<template>
+    <a :href="buttonProps.link" class="font-bold transition-all hover:text-[#00A7E3]">{{ buttonProps.text }}</a>
+</template>
