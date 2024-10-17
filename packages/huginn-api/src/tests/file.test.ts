22041b73a638c537ba7d93d2603d8470d1615edc
Matin Tat <matin.tat85@gmail.com>
2024-10-17 18:28:01 +0200

    feat(api): left over from last commit

--- packages/huginn-api/src/tests/file.test.ts
+++ packages/huginn-api/src/tests/file.test.ts
@@ -1,10 +1,11 @@
 import { describe, expect, test } from "bun:test";
 import path from "node:path";
-import { resolveBase64, resolveFile, resolveImage } from "@huginn/shared";
+import { resolveFile, resolveImage, toDataUrl } from "@huginn/shared";
+import { decodeBase64 } from "@std/encoding";
 
 describe("file", () => {
 	test("file-resolve-path", async () => {
-		const result = resolveBase64((await resolveFile(path.join(__dirname, "pixel.png"))).data);
+		const result = toDataUrl((await resolveFile(path.join(__dirname, "pixel.png")))!.data);
 
 		expect(result).toBe(
 			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAANSURBVBhXY2BgYPgPAAEEAQBwIGULAAAAAElFTkSuQmCC",
@@ -17,13 +18,12 @@ describe("file", () => {
 			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAANSURBVBhXY2BgYPgPAAEEAQBwIGULAAAAAElFTkSuQmCC",
 		);
 	});
-	test("image-resolve-buffer", async () => {
-		const buffer = Buffer.from(
+	test("image-resolve-base64", async () => {
+		const resource = decodeBase64(
 			"iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAANSURBVBhXY2BgYPgPAAEEAQBwIGULAAAAAElFTkSuQmCC",
-			"base64",
 		);
 
-		const result = resolveBase64((await resolveFile(buffer)).data);
+		const result = toDataUrl((await resolveFile(resource.buffer as ArrayBuffer))!.data);
 
 		expect(result).toBe(
 			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAANSURBVBhXY2BgYPgPAAEEAQBwIGULAAAAAElFTkSuQmCC",
