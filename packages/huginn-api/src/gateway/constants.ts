e439aac0b5be695ef20c0d92c7dc3c76b0cd6940
Matin Tat <matin.tat85@gmail.com>
2024-10-27 21:12:01 +0100

    fix(api): remove old option from default gateway options

--- packages/huginn-api/src/gateway/constants.ts
+++ packages/huginn-api/src/gateway/constants.ts
@@ -2,7 +2,6 @@ import type { GatewayOptions } from "../types";
 
 export const DefaultGatewayOptions: Required<GatewayOptions> = {
 	url: "ws://localhost:3000/gateway",
-	identify: true,
 	log: true,
 	createSocket(_url) {
 		console.error("createSocket function is not implemented. Please implement it using your own way");
