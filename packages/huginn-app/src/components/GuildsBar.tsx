845e12bf95475fead888133bb32838bbf6f7b3dc
Matin Tat <matin.tat85@gmail.com>
2024-12-26 21:08:28 +0100

    feat(app): notification button sorting

--- packages/huginn-app/src/components/GuildsBar.tsx
+++ packages/huginn-app/src/components/GuildsBar.tsx
@@ -1,15 +1,36 @@
+import { snowflake } from "@huginn/shared";
+import { useQuery } from "@tanstack/react-query";
+import moment from "moment";
+
 export default function GuildsBar() {
+	const client = useClient();
 	const { readStates } = useReadStates();
+	const { data: channels } = useQuery(getChannelsOptions(client, "@me"));
+
+	const sortedReadStates = useMemo(
+		() =>
+			readStates
+				.filter((x) => x.unreadCount > 0)
+				.toSorted((a, b) => {
+					const aChannel = channels?.find((x) => x.id === a.channelId);
+					const bChannel = channels?.find((x) => x.id === b.channelId);
+
+					if (aChannel?.lastMessageId && bChannel?.lastMessageId) {
+						return moment(snowflake.getTimestamp(aChannel.lastMessageId)).isBefore(snowflake.getTimestamp(bChannel.lastMessageId)) ? 1 : -1;
+					}
+
+					return 0;
+				}),
+		[readStates],
+	);
 
 	return (
 		<nav className="flex h-full w-[4.75rem] shrink-0 flex-col bg-background p-3.5">
 			<HomeButton />
 			<div className="flex flex-col items-center justify-center">
-				{readStates
-					.filter((x) => x.unreadCount > 0)
-					.map((x) => (
-						<UnreadChannel key={x.channelId} channelId={x.channelId} unreadCount={x.unreadCount} />
-					))}
+				{sortedReadStates.map((x) => (
+					<UnreadChannel key={x.channelId} channelId={x.channelId} unreadCount={x.unreadCount} />
+				))}
 			</div>
 			<div className="mx-4 my-3.5 h-0.5 bg-white/20" />
 			<div className="flex flex-col items-center gap-3">
--- packages/huginn-app/src/components/UnreadChannel.tsx
+++ packages/huginn-app/src/components/UnreadChannel.tsx
@@ -2,6 +2,7 @@ import { ChannelType, type Snowflake } from "@huginn/shared";
 
 export default function UnreadChannel(props: { channelId: Snowflake; unreadCount: number }) {
 	const channel = useChannel(props.channelId);
+	const channelName = useChannelName(channel?.recipients, channel?.name);
 	const navigateToChannel = useNavigateToChannel();
 
 	if (!channel) {
@@ -9,19 +10,21 @@ export default function UnreadChannel(props: { channelId: Snowflake; unreadCount
 	}
 
 	return (
-		<button
-			type="button"
-			className="relative mt-3 flex items-center rounded-lg hover:bg-secondary/10"
-			onClick={() => navigateToChannel("@me", channel.id)}
-		>
-			{channel.type === ChannelType.DM ? (
-				<UserAvatar userId={channel.recipients[0]?.id} avatarHash={channel.recipients[0]?.avatar} size="3rem" hideStatus />
-			) : (
-				<ChannelIcon channelId={channel?.id} iconHash={channel?.icon} size="3rem" />
-			)}
-			<div className="absolute right-0 bottom-0 h-5 w-5 rounded-full bg-error font-bold text-sm">
-				<div className="text-white">{props.unreadCount}</div>
-			</div>
-		</button>
+		<Tooltip placement="right">
+			<Tooltip.Trigger
+				className="relative mt-3 flex items-center rounded-lg hover:bg-secondary/10"
+				onClick={() => navigateToChannel("@me", channel.id)}
+			>
+				{channel.type === ChannelType.DM ? (
+					<UserAvatar userId={channel.recipients[0]?.id} avatarHash={channel.recipients[0]?.avatar} size="3rem" hideStatus />
+				) : (
+					<ChannelIcon channelId={channel?.id} iconHash={channel?.icon} size="3rem" />
+				)}
+				<div className="absolute right-0 bottom-0 h-5 w-5 rounded-full bg-error font-bold text-sm">
+					<div className="text-white">{props.unreadCount}</div>
+				</div>
+			</Tooltip.Trigger>
+			<Tooltip.Content>{channelName}</Tooltip.Content>
+		</Tooltip>
 	);
 }
