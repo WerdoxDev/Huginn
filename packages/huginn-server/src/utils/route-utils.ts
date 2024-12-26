9289f39e2a99ccdcc744ba8a0c63509eb791aa2d
Matin Tat <matin.tat85@gmail.com>
2024-12-26 10:29:21 +0100

    feat(server): remove nightly from app releases endpoints

--- packages/huginn-server/src/utils/route-utils.ts
+++ packages/huginn-server/src/utils/route-utils.ts
@@ -2,7 +2,10 @@ import { type ErrorFactory, createErrorFactory, unauthorized } from "@huginn/bac
 import { Errors, HttpCode, type IdentityTokenPayload, type TokenPayload, type Unpacked } from "@huginn/shared";
 import type { Endpoints } from "@octokit/types";
 import { type H3Event, getHeader, setResponseStatus } from "h3";
+import * as semver from "semver";
 import { type DBError, DBErrorType, prisma } from "#database";
+import { octokit } from "#server";
+import { envs } from "#setup";
 import { verifyToken } from "./token-factory";
 
 export async function useVerifiedJwt<IdentityToken extends boolean = false>(event: H3Event, identity?: IdentityToken) {
@@ -60,4 +63,15 @@ export function handleCommonDBErrors(event: H3Event, error: DBError) {
 
 export function getWindowsAssetUrl(release?: Unpacked<Endpoints["GET /repos/{owner}/{repo}/releases"]["response"]["data"]>) {
 	return release?.assets.find((x) => x.name.endsWith("setup.exe"))?.browser_download_url;
+}
+
+export function getAppPackageVersion(tagName: string) {
+	return tagName.replace("huginn/app-", "");
+}
+
+export async function getAllAppReleases() {
+	const releases = (await octokit.rest.repos.listReleases({ owner: envs.REPO_OWNER, repo: envs.REPO })).data
+		.filter((x) => x.tag_name.includes("huginn/app"))
+		.sort((v1, v2) => semver.rcompare(getAppPackageVersion(v1.tag_name), getAppPackageVersion(v2.tag_name)));
+	return releases;
 }
