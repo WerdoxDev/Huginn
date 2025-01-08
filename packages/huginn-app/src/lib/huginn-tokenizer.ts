7d3581874549abd387db06a1677c8c14a9f70e1f
Matin Tat <matin.tat85@gmail.com>
2025-01-08 13:21:47 +0100

    fix(app): link rendering with interfering markdown should be prioritized

--- packages/huginn-app/src/lib/huginn-tokenizer.ts
+++ packages/huginn-app/src/lib/huginn-tokenizer.ts
@@ -42,33 +42,63 @@ export function tokenize(text: string) {
 	const tokens: FinishedToken[] = [];
 
 	let match: RegExpExecArray | null;
-	const pattern =
-		/(?<link>https?:\/\/[^\s/$.?#].[^\s]*)|(?<threes>(\*\*\*))|(?<twos>(\*\*))|(?<ones>(\*))|(?<threeu>(\_\_\_))|(?<twou>(\_\_))|(?<oneu>(\_))|(?<twol>(\|\|))/g;
+	const patterns = [
+		/(?<threes>(\*\*\*))/g,
+		/(?<twos>(\*\*))/g,
+		/(?<ones>(\*))/g,
+		/(?<threeu>(\_\_\_))/g,
+		/(?<twou>(\_\_))/g,
+		/(?<oneu>(\_))/g,
+		/(?<twol>(\|\|))/g,
+		/(?<link>https?:\/\/[^\s/$.?#].[^\s]*)/g,
+	];
 
 	const unfinishedTokens: { start?: Token; end?: Token; type: TokenTypeFlag }[] = [];
-	// biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
-	while ((match = pattern.exec(text)) !== null) {
-		for (const [group, type] of Object.entries(tokenTypes)) {
-			if (match.groups?.[group]) {
-				const unfinishedTokenIndex = unfinishedTokens.findIndex((x) => x.type === type);
+	for (const pattern of patterns) {
+		// console.log(pattern);
+		// biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
+		while ((match = pattern.exec(text)) !== null) {
+			// for (const [group, type] of Object.entries(tokenTypes)) {
+			const type = tokenTypes[Object.keys(match.groups ?? {})[0]];
+			if (!type) {
+				continue;
+			}
+			// if (match.groups?.[group]) {
+			// console.log([...tokens], match.index, match[0].length, match.groups);
+			if (
+				tokens.some(
+					(x) =>
+						((match?.index ?? 0) >= x.start && (match?.index ?? 0) <= x.start + (x.mark?.length ?? 0) - 1) ||
+						((match?.index ?? 0) <= x.end && (match?.index ?? 0) >= x.end - (x.mark?.length ?? 0) + 1),
+				)
+			) {
+				continue;
+			}
 
-				let token = unfinishedTokens[unfinishedTokenIndex];
-				if (hasFlag(type, TokenTypeFlag.LINK)) {
-					token = {
-						start: { index: match.index, text: "" },
-						end: { index: match.index + match[0].length, text: "" },
-						type: type,
-					};
-				}
+			const unfinishedTokenIndex = unfinishedTokens.findIndex((x) => x.type === type);
 
-				if (!token) {
-					unfinishedTokens.push({ start: { index: match.index, text: match[0] }, type: type });
-				} else if (token && !token.end) {
-					token.end = { index: match.index, text: match[0] };
-				}
+			let token = unfinishedTokens[unfinishedTokenIndex];
+			if (hasFlag(type, TokenTypeFlag.LINK)) {
+				const endIndex = match.index + match[0].length;
+				const interferingToken = tokens.find((x) => x.end === endIndex - 1);
+				token = {
+					start: { index: match.index, text: "" },
+					end: { index: endIndex - (interferingToken?.mark?.length ?? 0), text: "" },
+					type: type,
+				};
+			}
 
-				if (token?.start && token?.end) {
-					const content = text.slice(token.start.index + token.start.text.length, token.end.index);
+			// Token does not exist
+			if (!token) {
+				unfinishedTokens.push({ start: { index: match.index, text: match[0] }, type: type });
+			} else if (token && !token.end) {
+				token.end = { index: match.index, text: match[0] };
+			}
+
+			// Check token is complete
+			if (token?.start && token?.end) {
+				const content = text.slice(token.start.index + token.start.text.length, token.end.index);
+				if (content) {
 					tokens.push({
 						start: token.start.index,
 						content,
@@ -76,12 +106,27 @@ export function tokenize(text: string) {
 						type: getTokenTypeFromFlag(token.type),
 						mark: token.start.text,
 					});
+				}
 
-					if (unfinishedTokenIndex !== -1) {
-						unfinishedTokens.splice(unfinishedTokenIndex, 1);
-					}
+				if (!content) {
+					unfinishedTokens.push({ start: token.end, type: token.type });
+				}
+
+				if (unfinishedTokenIndex !== -1) {
+					unfinishedTokens.splice(unfinishedTokenIndex, 1);
 				}
 			}
+		}
+
+		unfinishedTokens.splice(0, unfinishedTokens.length);
+	}
+
+	// Aftermath
+	for (const linkToken of tokens.filter((x) => x.type.includes("link"))) {
+		const interferingTokenIndex = tokens.findIndex((x) => x.start > linkToken.start && x.end < linkToken.end);
+
+		if (interferingTokenIndex !== -1) {
+			tokens.splice(interferingTokenIndex, 1);
 		}
 	}
 
@@ -116,6 +161,8 @@ export function mergeTokens(tokens: FinishedToken[]) {
 	if (currentToken) {
 		mergedTokens.push(currentToken);
 	}
+
+	// console.log(mergedTokens);
 
 	return mergedTokens;
 }
