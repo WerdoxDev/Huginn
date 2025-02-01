33b95f26ec35c97acadbbee931930ae6d12460ba
Matin Tat <matin.tat85@gmail.com>
2025-02-01 20:49:31 +0100

    fix(bifrost): add cdn route handling + better websocket handling

--- packages/huginn-bifrost/src/index.ts
+++ packages/huginn-bifrost/src/index.ts
@@ -1,7 +1,10 @@
 import type { WebSocketHandler } from "bun";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+
+app.use(cors());
 
 // Route requests based on the path
 app.all("/api/*", async (c) => {
@@ -10,28 +13,26 @@ app.all("/api/*", async (c) => {
 	return fetch(targetUrl, c.req.raw);
 });
 
-const websocketHandler: WebSocketHandler<{ url: string; client: WebSocket | undefined }> = {
+app.all("/*", async (c) => {
+	const targetUrl = `http://localhost:3002${c.req.path}`;
+
+	return fetch(targetUrl, c.req.raw);
+});
+
+app.onError((error, c) => {
+	console.log("Error on", c.req.path);
+	return c.text("An endpoint is not connected");
+});
+
+const websocketHandler: WebSocketHandler<{ url: string; client: WebSocket }> = {
 	open(serverSocket) {
 		console.log("WebSocket connection established:", serverSocket.data.url);
 
-		// Determine target server based on path
-		let targetUrl: string | undefined;
-		if (serverSocket.data.url.startsWith("http://localhost:3001/gateway")) {
-			targetUrl = "ws://localhost:3004/gateway";
-		} else {
-			serverSocket.close(4001, "Unknown WebSocket route");
-			return;
-		}
-
-		// Create a WebSocket connection to the target server
-		const clientSocket = new WebSocket(targetUrl);
-		serverSocket.data.client = clientSocket;
-
-		clientSocket.onmessage = (event) => {
+		serverSocket.data.client.onmessage = (event) => {
 			serverSocket.send(event.data);
 		};
 
-		clientSocket.onclose = (event) => {
+		serverSocket.data.client.onclose = (event) => {
 			serverSocket.close(event.code, event.reason);
 		};
 	},
@@ -46,10 +47,25 @@ const websocketHandler: WebSocketHandler<{ url: string; client: WebSocket | unde
 };
 
 Bun.serve({
-	fetch(request, server) {
+	async fetch(request, server) {
 		if (request.url.includes("gateway")) {
-			if (server.upgrade(request, { data: { url: request.url, client: undefined } })) {
+			try {
+				const targetUrl = "ws://localhost:3004/gateway";
+				const clientSocket = new WebSocket(targetUrl);
+				await new Promise((res, rej) => {
+					clientSocket.onopen = () => {
+						if (server.upgrade(request, { data: { url: request.url, client: clientSocket } })) {
+							res(undefined);
+							return;
+						}
+					};
+					clientSocket.onerror = () => {
+						rej(undefined);
+					};
+				});
 				return;
+			} catch (e) {
+				return new Response("Upgrade failed", { status: 400 });
 			}
 		}
 		return app.fetch(request, server);
