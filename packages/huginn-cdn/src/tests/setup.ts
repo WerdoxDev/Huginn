143b16842312fe19d44b871d14c1d06bbdf43f43
Matin Tat <matin.tat85@gmail.com>
2025-03-06 14:23:26 +0100

    fix(cdn): remove test flag from cdn as it is now in backend-shared

--- packages/huginn-cdn/src/tests/setup.ts
+++ packages/huginn-cdn/src/tests/setup.ts
@@ -2,6 +2,5 @@ import { beforeAll } from "bun:test";
 import { prepareServer } from "@huginn/backend-shared";
 
 beforeAll(async () => {
-	process.env.test = JSON.stringify(true);
 	await prepareServer();
 });
