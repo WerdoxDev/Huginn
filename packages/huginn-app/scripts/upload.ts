df2f245d3ab5cef4c16cb789379d19f1c67fadff
Matin Tat <matin.tat85@gmail.com>
2025-04-11 08:14:48 +0200

    feat(app): electron github action #1

--- packages/huginn-app/scripts/upload.ts
+++ packages/huginn-app/scripts/upload.ts
@@ -1,9 +1,9 @@
 // upload-to-release.ts
-import { version } from "./package.json";
-const GITHUB_TOKEN = "";
+import { version } from "../package.json";
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const OWNER = "WerdoxDev";
 const REPO = "Huginn";
-const TAG = `app@v${version}`;
+const TAG = `app2@v${version}`;
 
 const FILES = [
 	{
