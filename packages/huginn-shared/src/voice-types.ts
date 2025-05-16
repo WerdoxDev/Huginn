ed3009283881ef9f8ce947c0843b18f3c752d980
Matin Tat <matin.tat85@gmail.com>
2025-05-16 13:23:39 +0200

    feat(shared): add local voice state change event

--- packages/huginn-shared/src/voice-types.ts
+++ packages/huginn-shared/src/voice-types.ts
@@ -56,6 +56,7 @@ export type VoiceEvents = {
 	connected: undefined;
 	disconnected: undefined;
 	ping: { rtt: number };
+	local_voice_state_changed: { audioPaused: boolean; audioMuted: boolean; consumersMuted: boolean };
 };
 
 export type VoicePayload<OP extends keyof VoiceOperationDatas | undefined = undefined> = {
