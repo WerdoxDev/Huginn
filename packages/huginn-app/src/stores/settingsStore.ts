04da7fc9cc04d14cc8e5d09330486202915f471e
Matin Tat <matin.tat85@gmail.com>
2025-07-15 17:20:51 +0200

    chore(app): rename some setting fields

--- packages/huginn-app/src/stores/settingsStore.ts
+++ packages/huginn-app/src/stores/settingsStore.ts
@@ -1,17 +1,13 @@
-import { error } from "@huginn/shared";
-import { messages } from "@lib/error-messages";
 import { createStore, useStore } from "zustand";
 import { combine } from "zustand/middleware";
 import type { ThemeType } from "@/types";
-import { apiStore } from "./apiStore";
-import { modalsStore } from "./modalsStore";
 
 export type AppSettings = {
    apiHostname: string;
    cdnHostname: string;
    voiceHostname: string;
-   externalUrl: string;
-   hostnameMode: "manual" | "external"
+   externalHostnamesUrl: string;
+   hostnameSource: "manual" | "external"
    theme: ThemeType;
    inputDeviceId: string;
    outputDeviceId: string;
@@ -29,8 +25,8 @@ const initialStore = () =>
       apiHostname: "https://midgard.huginn.dev",
       cdnHostname: "https://midgard.huginn.dev",
       voiceHostname: "https://midgard.huginn.dev",
-      hostnameMode: "manual",
-      externalUrl: "",
+      hostnameSource: "manual",
+      externalHostnamesUrl: "",
       theme: "pine green",
       inputDeviceId: "",
       outputDeviceId: "",
@@ -57,27 +53,6 @@ export async function initializeSettings() {
       window.localStorage.setItem(localStorageItem, JSON.stringify(initialValue));
       // biome-ignore lint/style/noNonNullAssertion: the local storage item is checked before
       store.setState({ ...initialValue, ...JSON.parse(globalThis.localStorage.getItem(localStorageItem)!) });
-   }
-
-   const thisStore = store.getState();
-   if (thisStore.hostnameMode === "manual") {
-      apiStore.setState({ hostnames: { api: thisStore.apiHostname, cdn: thisStore.cdnHostname, voice: thisStore.voiceHostname } });
-   } else {
-      let response: Response | undefined;
-      try {
-         response = (await fetch(thisStore.externalUrl, { cache: "no-cache" }));
-         const json = await response?.json();
-         apiStore.setState({ hostnames: { api: json.api, cdn: json.cdn, voice: json.voice } });
-      } catch (e) {
-         error("app:settings-store", "Error fetching external hostnames", e);
-
-         modalsStore.getState().updateModals({ info: { isOpen: true, ...messages.externalUrlError(), status: "error" } })
-      }
-   }
-
-   if (window.electronAPI) {
-      const url = `${apiStore.getState().hostnames.api}/api/update/win`;
-      window.electronAPI.setUpdateUrl(url);
    }
 }
 
