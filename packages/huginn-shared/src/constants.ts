a44bcf354c47ef252c86f469a3bd57bb7c5417a6
Matin Tat <matin.tat85@gmail.com>
2025-07-16 23:07:49 +0200

    fix(shared): add voice token expire time to constants

--- packages/huginn-shared/src/constants.ts
+++ packages/huginn-shared/src/constants.ts
@@ -7,6 +7,7 @@ export const constants = {
    IDENTITY_TOKEN_EXPIRE_TIME: "5mins",
    ACCESS_TOKEN_EXPIRE_TIME: "7d",
    REFRESH_TOKEN_EXPIRE_TIME: "14d",
+   VOICE_TOKEN_EXPIRE_TIME: "20secs",
    EMAIL_REGEX:
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ as RegExp,
    USERNAME_REGEX: /^[a-zA-Z0-9_.]*$/ as RegExp,
