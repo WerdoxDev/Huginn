34b068b245411450c4fe8b8d54467bf2d3c88600
Matin Tat <matin.tat85@gmail.com>
2025-07-18 17:46:37 +0200

    fix: make deploy.bat show full status

--- deploy.bat
+++ deploy.bat
@@ -1,22 +1,22 @@
 @echo off
 
 echo *> .dockerignore
-docker build -t ghcr.io/werdoxdev/caddy -f Dockerfile.caddy --progress=plain .
+docker build -t ghcr.io/werdoxdev/caddy -f Dockerfile.caddy .
 git checkout .dockerignore
 
 echo **/huginn-cdn>> .dockerignore
 echo **/huginn-voice>> .dockerignore
-docker build -t ghcr.io/werdoxdev/huginn-server -f Dockerfile.huginn-server --progress=plain .
+docker build -t ghcr.io/werdoxdev/huginn-server -f Dockerfile.huginn-server .
 git checkout .dockerignore
 
 echo **/huginn-server>> .dockerignore
 echo **/huginn-voice>> .dockerignore
-docker build -t ghcr.io/werdoxdev/huginn-cdn -f Dockerfile.huginn-cdn --progress=plain .
+docker build -t ghcr.io/werdoxdev/huginn-cdn -f Dockerfile.huginn-cdn .
 git checkout .dockerignore
 
 echo **/huginn-server>> .dockerignore
 echo **/huginn-cdn>> .dockerignore
-docker build -t ghcr.io/werdoxdev/huginn-voice -f Dockerfile.huginn-voice --progress=plain .
+docker build -t ghcr.io/werdoxdev/huginn-voice -f Dockerfile.huginn-voice .
 git checkout .dockerignore
 
 docker push ghcr.io/werdoxdev/caddy:latest
