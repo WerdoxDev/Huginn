c7d4bdbb8d6df04bbf575f664df587b05b92fb9a
Matin Tat <matin.tat85@gmail.com>
2025-07-27 19:28:07 +0200

    chore(api): lf to crlf

--- packages/huginn-api/src/rest-apis/user.ts
+++ packages/huginn-api/src/rest-apis/user.ts
@@ -1,31 +1,31 @@
 import {
-	type APIGetCurrentUserResult,
-	type APIGetUserResult,
-	type APIPatchCurrentUserJSONBody,
-	type APIPatchCurrentUserResult,
-	Routes,
-	type Snowflake,
-	resolveImage,
+   type APIGetCurrentUserResult,
+   type APIGetUserByIdResult,
+   type APIPatchCurrentUserJSONBody,
+   type APIPatchCurrentUserResult,
+   Routes,
+   type Snowflake,
+   resolveImage,
 } from "@huginn/shared";
 import type { REST } from "../rest";
 
 export class UserAPI {
-	private readonly rest: REST;
+   private readonly rest: REST;
 
-	public constructor(rest: REST) {
-		this.rest = rest;
-	}
+   public constructor(rest: REST) {
+      this.rest = rest;
+   }
 
-	public async get(userId: Snowflake): Promise<APIGetUserResult> {
-		return this.rest.get(Routes.user(userId), { auth: true }) as Promise<APIGetUserResult>;
-	}
+   public async get(userId: Snowflake): Promise<APIGetUserByIdResult> {
+      return this.rest.get(Routes.user(userId), { auth: true }) as Promise<APIGetUserByIdResult>;
+   }
 
-	public async getCurrent(): Promise<APIGetCurrentUserResult> {
-		return this.rest.get(Routes.user("@me"), { auth: true }) as Promise<APIGetCurrentUserResult>;
-	}
+   public async getCurrent(): Promise<APIGetCurrentUserResult> {
+      return this.rest.get(Routes.user("@me"), { auth: true }) as Promise<APIGetCurrentUserResult>;
+   }
 
-	public async edit(body: APIPatchCurrentUserJSONBody): Promise<APIPatchCurrentUserResult> {
-		const resolvedBody: APIPatchCurrentUserJSONBody = { ...body, avatar: body.avatar && (await resolveImage(body.avatar)) };
-		return this.rest.patch(Routes.user("@me"), { body: resolvedBody, auth: true }) as Promise<APIPatchCurrentUserResult>;
-	}
+   public async edit(body: APIPatchCurrentUserJSONBody): Promise<APIPatchCurrentUserResult> {
+      const resolvedBody: APIPatchCurrentUserJSONBody = { ...body, avatar: body.avatar && (await resolveImage(body.avatar)) };
+      return this.rest.patch(Routes.user("@me"), { body: resolvedBody, auth: true }) as Promise<APIPatchCurrentUserResult>;
+   }
 }
--- packages/huginn-api/src/tests/file.test.ts
+++ packages/huginn-api/src/tests/file.test.ts
@@ -4,29 +4,29 @@ import { resolveFile, resolveImage, toDataUrl } from "@huginn/shared";
 import { decodeBase64 } from "@std/encoding";
 
 describe("file resolver", () => {
-	test("should resolve a file using it's system path", async () => {
-		const result = toDataUrl((await resolveFile(path.join(__dirname, "pixel.png")))!.data);
+   test("should resolve a file using it's system path", async () => {
+      const result = toDataUrl((await resolveFile(path.join(__dirname, "pixel.png")))!.data, "png");
 
-		expect(result).toBe(
-			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAANSURBVBhXY2BgYPgPAAEEAQBwIGULAAAAAElFTkSuQmCC",
-		);
-	});
-	test("should return a data url image from system path", async () => {
-		const result = await resolveImage(path.join(__dirname, "pixel.png"));
+      expect(result).toBe(
+         "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAANSURBVBhXY2BgYPgPAAEEAQBwIGULAAAAAElFTkSuQmCC",
+      );
+   });
+   test("should return a data url image from system path", async () => {
+      const result = await resolveImage(path.join(__dirname, "pixel.png"));
 
-		expect(result).toBe(
-			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAANSURBVBhXY2BgYPgPAAEEAQBwIGULAAAAAElFTkSuQmCC",
-		);
-	});
-	test("should create a data url from a base64 string", async () => {
-		const resource = decodeBase64(
-			"iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAANSURBVBhXY2BgYPgPAAEEAQBwIGULAAAAAElFTkSuQmCC",
-		);
+      expect(result).toBe(
+         "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAANSURBVBhXY2BgYPgPAAEEAQBwIGULAAAAAElFTkSuQmCC",
+      );
+   });
+   test("should create a data url from a base64 string", async () => {
+      const resource = decodeBase64(
+         "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAANSURBVBhXY2BgYPgPAAEEAQBwIGULAAAAAElFTkSuQmCC",
+      );
 
-		const result = toDataUrl((await resolveFile(resource.buffer as ArrayBuffer))!.data);
+      const result = toDataUrl((await resolveFile(resource.buffer as ArrayBuffer))!.data, "png");
 
-		expect(result).toBe(
-			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAANSURBVBhXY2BgYPgPAAEEAQBwIGULAAAAAElFTkSuQmCC",
-		);
-	});
+      expect(result).toBe(
+         "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAANSURBVBhXY2BgYPgPAAEEAQBwIGULAAAAAElFTkSuQmCC",
+      );
+   });
 });
