e18b0d4662a32fbb855ffad1b45840eabad1d292
Matin Tat <matin.tat85@gmail.com>
2025-09-05 09:14:44 +0200

    chore(server): use a token for cdn upload requests

--- packages/huginn-server/src/utils/server-request.ts
+++ packages/huginn-server/src/utils/server-request.ts
@@ -1,39 +1,57 @@
-import { logCDNRequest } from "@huginn/backend-shared";
-import { HTTPError, type InternalRequest, type RequestData, type RequestMethod, type RouteLike, parseResponse, resolveRequest } from "@huginn/shared";
+import { createToken, logCDNRequest } from "@huginn/backend-shared";
+import {
+   HTTPError,
+   type InternalRequest,
+   type RequestData,
+   type RequestMethod,
+   type RouteLike,
+   constants,
+   parseResponse,
+   resolveRequest,
+} from "@huginn/shared";
 import { envs } from "#setup";
 
 export async function cdnUpload<T>(fullRoute: RouteLike, options: RequestData = {}) {
-	if (!envs.CDN_LOCAL_URL) {
-		throw new Error("CDN Root was not configured");
-	}
+   if (!envs.CDN_LOCAL_URL) {
+      throw new Error("CDN Root was not configured");
+   }
 
-	logCDNRequest(fullRoute, "POST");
-	return (await request({ ...options, root: envs.CDN_LOCAL_URL, method: "POST", fullRoute, throw: true })) as Promise<T>;
+   const token = await createToken("cdn", undefined, constants.CDN_TOKEN_EXPIRE_TIME);
+
+   logCDNRequest(fullRoute, "POST");
+   return (await request({
+      ...options,
+      root: envs.CDN_LOCAL_URL,
+      method: "POST",
+      fullRoute,
+      throw: true,
+      headers: { ...options.headers, Authorization: `Bearer ${token}` },
+   })) as Promise<T>;
 }
 
 export async function serverFetch<T>(url: string, method: RequestMethod, options: RequestData & { throw?: boolean }) {
-	const fullUrl = new URL(url);
-	return (await request({
-		...options,
-		root: fullUrl.origin,
-		fullRoute: fullUrl.pathname as `/${string}`,
-		method,
-		token: options.token,
-		auth: options.token !== undefined,
-		authPrefix: "Bearer",
-	})) as Promise<T>;
+   const fullUrl = new URL(url);
+   return (await request({
+      ...options,
+      root: fullUrl.origin,
+      fullRoute: fullUrl.pathname as `/${string}`,
+      method,
+      token: options.token,
+      auth: options.token !== undefined,
+      authPrefix: "Bearer",
+   })) as Promise<T>;
 }
 
 export async function request(options: InternalRequest & { throw?: boolean }): Promise<unknown> {
-	const { url, fetchOptions } = await resolveRequest(options);
+   const { url, fetchOptions } = await resolveRequest(options);
 
-	const response = await fetch(url, fetchOptions);
+   const response = await fetch(url, fetchOptions);
 
-	if (response.ok || !options.throw) return parseResponse(response);
+   if (response.ok || !options.throw) return parseResponse(response);
 
-	if (response.status >= 400 && response.status < 600) {
-		throw new HTTPError(response.status, response.statusText, options.method, url, fetchOptions);
-	}
+   if (response.status >= 400 && response.status < 600) {
+      throw new HTTPError(response.status, response.statusText, options.method, url, fetchOptions);
+   }
 
-	return response;
+   return response;
 }
