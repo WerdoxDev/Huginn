281385afba6d3adccf6ff3cd3dad239c2c1ab4cd
Matin Tat <matin.tat85@gmail.com>
2025-09-14 10:47:23 +0200

    feat(api): add submit known application method

--- packages/huginn-api/src/rest-apis/application.ts
+++ packages/huginn-api/src/rest-apis/application.ts
@@ -1,4 +1,11 @@
-import { Routes, type APIGetKnownApplicationsResult, type APIPostApplicationIconJSONBody, type APIPostApplicationIconResult } from "@huginn/shared";
+import {
+   Routes,
+   type APIGetKnownApplicationsResult,
+   type APIPostApplicationIconJSONBody,
+   type APIPostApplicationIconResult,
+   type APIPostKnownApplicationJSONBody,
+   type APIPostKnownApplicationResult,
+} from "@huginn/shared";
 import type { REST } from "../rest";
 
 export class ApplicationAPI {
@@ -13,6 +20,10 @@ export class ApplicationAPI {
          auth: true,
          query: since ? new URLSearchParams({ since: since.getTime().toString() }) : undefined,
       }) as Promise<APIGetKnownApplicationsResult>;
+   }
+
+   public async submitKnown(body: APIPostKnownApplicationJSONBody): Promise<APIPostKnownApplicationResult> {
+      return this.rest.post(Routes.knownApplications(), { auth: true, body }) as Promise<APIPostKnownApplicationResult>;
    }
 
    public async uploadIcon(body: APIPostApplicationIconJSONBody): Promise<APIPostApplicationIconResult> {
