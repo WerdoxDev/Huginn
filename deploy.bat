0ec72945c7f89461dceddf1bcaa03694723db722
Matin Tat <matin.tat85@gmail.com>
2025-09-24 14:18:44 +0200

    chore: separate dockerignore files for each Dockerfile

--- deploy.bat
+++ deploy.bat
@@ -1,23 +1,10 @@
 @echo off
 
-echo *> .dockerignore
 docker build -t ghcr.io/werdoxdev/caddy -f Dockerfile.caddy .
-git checkout .dockerignore
 
-echo **/huginn-cdn>> .dockerignore
-echo **/huginn-voice>> .dockerignore
 docker build -t ghcr.io/werdoxdev/huginn-server -f Dockerfile.huginn-server .
-git checkout .dockerignore
-
-echo **/huginn-server>> .dockerignore
-echo **/huginn-voice>> .dockerignore
 docker build -t ghcr.io/werdoxdev/huginn-cdn -f Dockerfile.huginn-cdn .
-git checkout .dockerignore
-
-echo **/huginn-server>> .dockerignore
-echo **/huginn-cdn>> .dockerignore
 docker build -t ghcr.io/werdoxdev/huginn-voice -f Dockerfile.huginn-voice .
-git checkout .dockerignore
 
 docker push ghcr.io/werdoxdev/caddy:latest
 docker push ghcr.io/werdoxdev/huginn-server:latest
