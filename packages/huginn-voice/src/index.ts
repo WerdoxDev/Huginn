414680877d7ce4e60ce95926da040d76171c3f7e
Matin Tat <matin.tat85@gmail.com>
2025-10-23 20:48:37 +0200

    feat(voice): remove unnecessary http server for a websocket only server

--- packages/huginn-voice/src/index.ts
+++ packages/huginn-voice/src/index.ts
@@ -1,14 +1,11 @@
-import type { Server } from "node:http";
-import { serve } from "@hono/node-server";
 import { readEnv } from "@huginn/backend-shared/env-reader";
 import crossws from "crossws/adapters/node";
-import { Hono } from "hono";
-import { runMediasoupWorker } from "#mediasoup";
 import { VoiceWebsocket } from "./voice-websocket";
+import { createServer } from "node:http";
+import { runMediasoupWorker } from "#mediasoup";
+import type { AddressInfo } from "node:net";
 
 export const envs = readEnv(["VOICE_HOST", "VOICE_PORT", "MEDIA_IP", "MEDIA_ANNOUNCED_HOSTNAMES", "MEDIA_PORT"] as const);
-
-const app = new Hono();
 
 await runMediasoupWorker();
 
@@ -21,17 +18,11 @@ export const ws = crossws({
    },
 });
 
-const server = serve(
-   {
-      fetch: app.fetch,
-      hostname: envs.VOICE_HOST,
-      port: Number(envs.VOICE_PORT),
-   },
-   (info) => {
-      console.log(`Server is running on http://${info.address}:${info.port}`);
-   },
-) as Server;
-
+const server = createServer().listen(Number(envs.VOICE_PORT), envs.VOICE_HOST);
+server.on("listening", () => {
+   const info = server.address() as AddressInfo;
+   console.log(`Server is running on http://${info.address}:${info.port}`);
+});
 server.on("upgrade", (req, socket, head) => {
    if (req.headers.upgrade === "websocket") {
       ws.handleUpgrade(req, socket, head);
