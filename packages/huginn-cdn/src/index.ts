6ca665b6dfd563f219e9dd43fa322dce903019d0
Matin Tat <matin.tat85@gmail.com>
2025-12-14 18:38:51 +0100

    fix(cdn): fix potential static routes

--- packages/huginn-cdn/src/index.ts
+++ packages/huginn-cdn/src/index.ts
@@ -1,12 +1,52 @@
-import consola from "consola";
 import { envs } from "#setup";
-import { main } from "#elysia";
+import { postApplicationIcon } from "#routes/application-icons/[applicationId!].post";
+import { getApplicationIcon } from "#routes/application-icons/[applicationId!]/[iconHash].get";
+import { postMessageAttachment } from "#routes/attachments/[channelId]/[messageId].post";
+import { getMessageAttachment } from "#routes/attachments/[channelId]/[messageId]/[filename].get";
+import { postUserAvatar } from "#routes/avatars/[userId].post";
+import { getUserAvatar } from "#routes/avatars/[userId]/[avatarHash].get";
+import { postChannelIcon } from "#routes/channel-icons/[channelId].post";
+import { getChannelIcon } from "#routes/channel-icons/[channelId]/[iconHash].get";
+import cors from "@elysiajs/cors";
+import { cdnOnError, globalPlugin, invalidBody, notFound, serverError } from "@huginn/backend-shared";
+import consola from "consola";
+import Elysia from "elysia";
+import { getIndex } from "./routes";
 
-Bun.serve({
-   fetch: main.fetch,
-   hostname: envs.CDN_HOST,
-   port: envs.CDN_PORT,
-   idleTimeout: 40,
-});
+export const main = new Elysia()
+   .use(cors())
+   .use(globalPlugin)
+   .onError(({ error, code, status, path, request }) => {
+      consola.box(path, request.method, code, error);
+      if (code === "UNKNOWN") {
+         const returnedError = cdnOnError(error, status);
+         if (returnedError) {
+            return returnedError;
+         }
+      } else if (code === "VALIDATION" || code === "PARSE") {
+         return invalidBody(status);
+      } else if (code === "INTERNAL_SERVER_ERROR") {
+         return serverError(status);
+      } else if (code === "NOT_FOUND") {
+         return notFound(status);
+      }
 
-consola.box(`Listening on ${envs.CDN_HOST}:${envs.CDN_PORT}`);
+      return serverError(status);
+   })
+   .onAfterResponse(async ({ global }) => {
+      if (global.waitUntilPromises) {
+         await Promise.allSettled(global.waitUntilPromises.map((x) => x()) ?? []);
+      }
+   })
+   .use(getIndex)
+   .use(getApplicationIcon)
+   .use(postApplicationIcon)
+   .use(getMessageAttachment)
+   .use(postMessageAttachment)
+   .use(getUserAvatar)
+   .use(postUserAvatar)
+   .use(getChannelIcon)
+   .use(postChannelIcon)
+   .listen({ hostname: envs.CDN_HOST, port: envs.CDN_PORT, idleTimeout: 40 }, (server) => {
+      consola.box(`Listening on ${server.hostname}:${server.port}`);
+   });
