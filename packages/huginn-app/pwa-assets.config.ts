f4652a6ae39a87695c4aeefdee822eeca78fe43b
Matin Tat <matin.tat85@gmail.com>
2026-01-07 19:30:14 +0100

    fix(app): better pwa icons

--- packages/huginn-app/pwa-assets.config.ts
+++ packages/huginn-app/pwa-assets.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig, minimal2023Preset as preset } from "@vite-pwa/assets-generator/config";
 
 export default defineConfig({
-   preset: { apple: { sizes: [180] }, maskable: { sizes: [192, 512] }, transparent: { sizes: [64, 192, 512], favicons: [[96, "favicon.ico"]] } },
-   images: ["public/favicon.png"],
+   preset: {
+      apple: { sizes: [180], resizeOptions: { background: "#EBEBD3" } },
+      maskable: { sizes: [192, 512], resizeOptions: { background: "#EBEBD3" } },
+      transparent: { sizes: [64, 192, 512], favicons: [[96, "favicon.ico"]] },
+      png: { compressionLevel: 0, quality: 100, colors: 3 },
+   },
+   images: ["public/icon-outline.png"],
 });
