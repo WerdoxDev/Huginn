9ede7d923a037cc50d35d77b82dc30fb17e48d5a
Matin Tat <matin.tat85@gmail.com>
2026-01-24 13:53:29 +0100

    chore(backend-shared): cleanup prisma schema + add date fields to some important tables

--- packages/huginn-backend-shared/src/database/attachment.ts
+++ packages/huginn-backend-shared/src/database/attachment.ts
@@ -1,5 +1,5 @@
 import { WorkerID, snowflake } from "@huginn/shared";
-import { Prisma, prisma } from ".";
+import { Prisma, prisma } from "#database";
 
 export const attachmentExtension = Prisma.defineExtension({
    model: {
