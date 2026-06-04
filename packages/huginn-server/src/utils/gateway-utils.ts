9e2e90956fcc3029708031189ae4013b615a2a33
Matin Tat <matin.tat85@gmail.com>
2026-06-04 21:25:41 +0200

    feat(server): add OTel to some gateway utilities

--- packages/huginn-server/src/utils/gateway-utils.ts
+++ packages/huginn-server/src/utils/gateway-utils.ts
@@ -1,17 +1,30 @@
-import { gateway } from "#setup";
 // import { logGatewaySend } from "@huginn/backend-shared";
-import { type GatewayWebsocketEvents, GatewayOperations, type GatewayPayload } from "@huginn/shared";
+import { type GatewayWebsocketEvents, GatewayOperations, type GatewayPayload, analytics } from "@huginn/shared";
+
+import { gateway } from "#setup";
 
 export function dispatchToTopic<K extends keyof GatewayWebsocketEvents>(topics: string | string[], t: K, d: GatewayWebsocketEvents[K]) {
-   const data = { op: GatewayOperations.DISPATCH, t, d, s: 0 } as GatewayPayload;
+   analytics.startActiveSpan("gateway.dispatchToTopic", (span) => {
+      span.setAttributes({
+         "params.topic.count": Array.isArray(topics) ? topics.length : 1,
+         "params.event.type": t,
+      });
 
-   // logGatewaySend(topics, data, false);
+      try {
+         const data = { op: GatewayOperations.DISPATCH, t, d, s: 0 } as GatewayPayload;
 
-   if (Array.isArray(topics)) {
-      for (const topic of topics) {
-         gateway.sendToTopic(topic, data);
+         if (Array.isArray(topics)) {
+            for (const topic of topics) {
+               gateway.sendToTopic(topic, data);
+            }
+         } else {
+            gateway.sendToTopic(topics, data);
+         }
+      } catch (e) {
+         span.recordException(e as Error);
+         throw e;
+      } finally {
+         span.end();
       }
-   } else {
-      gateway.sendToTopic(topics, data);
-   }
+   });
 }
