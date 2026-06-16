a5cff1bca12548d24552585b779b0349fe370101
Matin Tat <matin.tat85@gmail.com>
2026-06-16 11:29:28 +0200

    fix: potential android workflow fix #8

--- packages/huginn-app/scripts/upload.ts
+++ packages/huginn-app/scripts/upload.ts
@@ -34,7 +34,7 @@ const FILES: Record<string, { path: string; name: string; type: string }[]> = {
    ],
    android: [
       {
-         path: `./android/app/outputs/apk/release/app-release.apk`,
+         path: `./android/app/build/outputs/apk/release/app-release.apk`,
          name: `Huginn_${version}.apk`,
          type: "application/octet-stream",
       },
