82dad7a69879928a9f6c68a9d3c67ec2cfe763af
Matin Tat <matin.tat85@gmail.com>
2026-06-26 10:50:23 +0200

    fix(app): many message box vertical alignment fixes

--- packages/huginn-app/src/components/editor/MessageEmojiElement.tsx
+++ packages/huginn-app/src/components/editor/MessageEmojiElement.tsx
@@ -1,12 +1,19 @@
 import { getEmojiFromSlug, getEmojiId } from "@huginn/shared";
+import clsx from "clsx";
 import { useMemo } from "react";
 
 export default function MessageEmojiElement(props: { emoji?: string; slug?: string; big?: boolean }) {
    const emoji = useMemo(() => props.emoji ?? (props.slug ? getEmojiFromSlug(props.slug) : undefined), [props.emoji, props.slug]);
    const src = useMemo(() => (emoji ? `${import.meta.env.BASE_URL}emojis/${getEmojiId(emoji)}.svg` : undefined), [emoji]);
    return (
-      <div className="inline-block align-middle">
-         <img src={src} draggable={false} data-type="emoji" alt={props.emoji} className={props.big ? "size-16" : "size-5"} />
+      <div className="inline-block align-baseline">
+         <img
+            src={src}
+            draggable={false}
+            data-type="emoji"
+            alt={props.emoji}
+            className={clsx("inline object-contain align-bottom", props.big ? "size-16" : "size-5.5")}
+         />
       </div>
    );
 }
