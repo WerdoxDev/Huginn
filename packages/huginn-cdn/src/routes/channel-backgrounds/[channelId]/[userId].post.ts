1be56764859af65fa90f8dfa204108d2dde165a5
Matin Tat <matin.tat85@gmail.com>
2026-08-10 21:00:36 +0200

    feat(cdn): rename channel id to scope

--- packages/huginn-cdn/src/routes/channel-backgrounds/[channelId]/[userId].post.ts
+++ packages/huginn-cdn/src/routes/channel-backgrounds/[channelId]/[userId].post.ts
@@ -6,15 +6,15 @@ import { storage } from "#server";
 const schema = t.Object({ files: t.Array(t.File()) });
 
 export const postChannelBackground = new Elysia().use(verifyJwt("cdn")).post(
-   "/cdn/channel-backgrounds/:channelId/:userId",
-   async ({ body, status, params: { channelId, userId } }) => {
+   "/cdn/channel-backgrounds/:scope/:userId",
+   async ({ body, status, params: { scope, userId } }) => {
       const file = body.files[0];
 
       if (!file) {
          return invalidBody(status);
       }
 
-      await storage.writeFile("channel-backgrounds", `${channelId}/${userId}`, file.name, file);
+      await storage.writeFile("channel-backgrounds", `${scope}/${userId}`, file.name, file);
 
       return status("Created", file.name);
    },
