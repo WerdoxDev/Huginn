468af092216f0731866b77f7affcb4ac48dc1299
Matin Tat <matin.tat85@gmail.com>
2026-09-16 12:52:33 +0200

    feat(server): add linux update support

--- packages/huginn-server/src/server.ts
+++ packages/huginn-server/src/server.ts
@@ -51,8 +51,8 @@ import { getLatestRelease } from "#routes/latest-release.get";
 import { postLog } from "#routes/log.post";
 import { getOnlineUsers } from "#routes/online-users.get";
 import { postUniqueUsername } from "#routes/unique-username.post";
+import { getDesktopUpdate } from "#routes/update/[linux|windows].get";
 import { getAndroidUpdate } from "#routes/update/android.get";
-import { getWinUpdate } from "#routes/update/win.get";
 import { getMe } from "#routes/users/@me.get";
 import { patchMe } from "#routes/users/@me.patch";
 import { getUserChannels } from "#routes/users/@me/channels.get";
@@ -209,7 +209,7 @@ export const app = new Elysia({
    .use(getLatestRelease)
    .use(getOnlineUsers)
    .use(postUniqueUsername)
-   .use(getWinUpdate)
+   .use(getDesktopUpdate)
    .use(getAndroidUpdate)
    .use(postLog)
    .use(getIndex)
